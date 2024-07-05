93864e61fb3f3cbf6ab9c9d9bd6da27c425964fe
Lukas Carvajal <lukascarvajal@gmail.com>
2024-07-05 16:54:53 +0200

    Add animated heart to open message

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Heart from "react-animated-heart";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [apiCalled, setApiCalled] = useState(false);
+  const [isClick, setClick] = useState(false);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (!apiCalled) {
+      setApiCalled(true);
+      fetch('https://for-diana-backend.vercel.app/thoughts')
+        .then((res) => res.json())
+        .then((data) => {
+          setMessage(data.thought);
+        });
+    }
+  }, []);
+
+  function handleClick() {
+    setClick(!isClick);
+    const timer = setTimeout(() => {
+      clearTimeout(timer);
+    }, 900);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>For Diana</h1>
+      {
+        message != "" && (
+          <>
+            <p>{message}</p>
+            <p>- Lukas</p>
+          </>
+        )
+      }
+      <Heart isClick={isClick} onClick={handleClick} />
     </>
   )
 }
